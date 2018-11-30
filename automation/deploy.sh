38f711834cbe924567aeb4de1f65afd2a1229863
Yohan Gouzerh <Yohan.Gouzerh@grenoble-inp.org>
2018-11-30 14:25:16 +0100

    Add workers

--- automation/deploy.sh
+++ automation/deploy.sh
@@ -31,3 +31,23 @@ echo "------ INSTALLATION --------"
 sudo ./common_install.sh
 sudo ./master_install.sh
 EOF
+
+echo "-- Workers configuration --"
+
+for worker_ip in $(./manage.py read type get-workers-public-ip)
+do
+  worker="ubuntu@$worker_ip"
+  echo "Copy configuration files to the worker"
+  scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./config/k8s_clus/kadmconf_wor.yml "$worker":~/
+  scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./automation/worker_install.sh "$worker":~/
+  scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./automation/common_install.sh "$worker":~/
+  echo "Connect to the worker to launch the installation phase"
+  echo "Connection to $worker"
+  # Launch the command on the master
+  ssh -T -o "StrictHostKeyChecking no" -i ssh/Smackey "$worker" << EOF
+  echo "------ INSTALLATION --------"
+  hostname
+  #sudo ./common_install.sh
+  #sudo ./worker_install.sh
+EOF
+done
