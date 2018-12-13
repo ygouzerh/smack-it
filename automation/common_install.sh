129aad40e40de17bfba3354c768c57da053d8fc7
ayoub mrini <ayoubmrini424@gmail.com>
2018-12-14 00:14:51 +0100

    Fixed some ssh issues
     Please enter the commit message for your changes. Lines starting

--- automation/common_install.sh
+++ automation/common_install.sh
@@ -7,6 +7,6 @@ sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl=1.13.0-00 docker.io
+sudo apt-get install -y kubelet kubeadm kubectl docker.io
 sudo apt-mark hold kubelet kubeadm kubectl docker.io
 sudo sysctl net.bridge.bridge-nf-call-iptables=1
