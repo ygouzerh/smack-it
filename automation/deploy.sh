c97a4f0d2a274f9520aff316f772799211563385
Yohan Gouzerh <Yohan.Gouzerh@grenoble-inp.org>
2018-12-14 17:09:32 +0100

    Start parallelization

--- automation/deploy.sh
+++ automation/deploy.sh
@@ -1,6 +1,92 @@
 #!/usr/bin/env bash
 
+##### FUNCTIONS #####
 
+## TRANSFER FILES FUNCTIONS
+transfer_files_on_master(){
+  # Transfer config files to the master
+  master=$1
+  echo "Deploy.sh : ----- COPY CONFIGURATION FILES (MASTER) : $master -----"
+  scp -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./config/k8s_clus/kadmconf_mas.yml "$master":~/ &
+  pid_one=$!
+  scp -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./automation/master_install.sh "$master":~/ &
+  pid_two=$!
+  scp -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./automation/common_install.sh "$master":~/ &
+  pid_three=$!
+  wait $pid_one
+  wait $pid_two
+  wait $pid_three
+}
+
+transfer_files_on_workers(){
+  # Transfer config files to the worker
+  for worker_ip in $(./manage.py read type get-workers-public-ip)
+  do
+    worker="ubuntu@$worker_ip"
+    echo "Deploy.sh : ---- COPY CONFIGURATION FILES (WORKER) : $worker_ip"
+    scp -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./config/k8s_clus/kadmconf_wor.yml "$worker":~/ &
+    pid_one=$!
+    scp -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./automation/worker_install.sh "$worker":~/ &
+    pid_two=$!
+    scp -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./automation/common_install.sh "$worker":~/ &
+    pid_three=$!
+    wait $pid_one
+    wait $pid_two
+    wait $pid_three
+  done
+}
+
+transfer_files(){
+  echo "Deploy.sh : ----- TRANSFERT FILES -----"
+  # Wrapper to transfer config files in parallel
+  transfer_files_on_master $1 &
+  pid_one=$!
+  transfer_files_on_workers &
+  pid_two=$!
+  wait $pid_one
+  wait $pid_two
+}
+
+## INSTALLATION FUNCTIONS
+install_master(){
+  master=$1
+  # Launch installation scripts on the master
+  echo "Deploy.sh : ----- MASTER INSTALLATION  -----"
+  ssh -o IdentitiesOnly=yes -T -o "StrictHostKeyChecking no" -i ssh/Smackey "$master" << EOF
+  sudo ./common_install.sh
+  sudo ./master_install.sh
+EOF
+}
+
+install_worker(){
+  # Launch installation scripts on one worker
+  worker_ip=$1
+  worker="ubuntu@$worker_ip"
+  echo "Deploy.sh : -- CONNECTION TO $worker -- "
+  ssh -o IdentitiesOnly=yes -T -o "StrictHostKeyChecking no" -i ssh/Smackey "$worker" << EOF
+  hostname
+  sudo ./common_install.sh
+  sudo ./worker_install.sh
+EOF
+}
+
+install_workers(){
+  # Launch the installation scripts on the worker
+  echo "Deploy.sh : ----- WORKER INSTALLATION -----"
+  for worker_ip in $(./manage.py read type get-workers-public-ip)
+  do
+    install_worker $worker_ip &
+    pid_one=$!
+    wait $pid_one
+  done
+}
+
+install(){
+  install_master $1
+  install_workers
+}
+
+##### MAIN #####
 
 # Go at the project's root
 cd ..
@@ -11,48 +97,20 @@ mkdir -p ssh/
 # Create the aws infrastructure
 ./manage.py install run
 
-# Get master
-echo "Retrieve the master..."
+# Retrieve the master
+echo "Deploy.sh : Retrieve the master..."
 master_public_ip=$(./manage.py read type get-master-public-ip)
 master="ubuntu@$master_public_ip"
 
 # Copy to be able to relaunch the ./deploy command infinitely
-echo "Save the kadm conf worker's file"
+echo "Deploy.sh : Save the kadm conf worker's file"
 cp ./config/k8s_clus/kadmconf_wor_default.yml ./config/k8s_clus/kadmconf_wor.yml
 
-echo "Add the master public ip to the kadm conf worker's file"
+echo "Deploy.sh : Add the master public ip to the kadm conf worker's file"
 echo "  - $master_public_ip:6443" >> ./config/k8s_clus/kadmconf_wor.yml
 
-echo "Copy configuration files to the master"
-scp -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./config/k8s_clus/kadmconf_mas.yml "$master":~/
-scp -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./automation/master_install.sh "$master":~/
-scp -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./automation/common_install.sh "$master":~/
+# Transfer the config files
+transfer_files $master
 
-echo "Connect to the master to launch the installation phase"
-echo "Connection to $master"
-# Launch the command on the master
-ssh -o IdentitiesOnly=yes -T -o "StrictHostKeyChecking no" -i ssh/Smackey "$master" << EOF
-echo "------ INSTALLATION --------"
-sudo ./common_install.sh
-sudo ./master_install.sh
-EOF
-
-echo "-- Workers configuration --"
-
-for worker_ip in $(./manage.py read type get-workers-public-ip)
-do
-  worker="ubuntu@$worker_ip"
-  echo "Copy configuration files to the worker"
-  scp -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./config/k8s_clus/kadmconf_wor.yml "$worker":~/
-  scp -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./automation/worker_install.sh "$worker":~/
-  scp -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ssh/Smackey ./automation/common_install.sh "$worker":~/
-  echo "Connect to the worker to launch the installation phase"
-  echo "Connection to $worker"
-  # Launch the command on the master
-  ssh -o IdentitiesOnly=yes -T -o "StrictHostKeyChecking no" -i ssh/Smackey "$worker" << EOF
-  echo "------ INSTALLATION --------"
-  hostname
-  sudo ./common_install.sh
-  sudo ./worker_install.sh
-EOF
-done
+# Install the master / workers
+install $master
--- automation/master_install.sh
+++ automation/master_install.sh
@@ -6,7 +6,10 @@ sudo kubeadm init --config kadmconf_mas.yml
 #To make kubectl work for your non-root user, run these commands, which are also part of the kubeadm init output:
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+USER_ID=$(id -u)
+USER_GROUP=$(id -g) 
+
+sudo chown "$USER_ID:$USER_GROUP" HOME/.kube/config
 # Install a pod network add-on (To allow the pods communicating with each others)
 sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
